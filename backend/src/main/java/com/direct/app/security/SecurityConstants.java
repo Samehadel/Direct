6b0c0b624417d3ba70aa4be9be4416494184be03
Samehadel <eng.sameh95@outlook.com>
2022-02-28 08:47:46 +0200

    Security Files Formatted

--- backend/src/main/java/com/direct/app/security/SecurityConstants.java
+++ backend/src/main/java/com/direct/app/security/SecurityConstants.java
@@ -2,12 +2,6 @@ package com.direct.app.security;
 
 public class SecurityConstants {
 	
-	//public static final String PASSWORD_RESET_REQUEST_URL = "/users/password-reset-request";
-	//public static final String VERIFICATION_EMAIL_URL = "/users/email-verification";
-	//	public static final long PASSWORD_RESET_EXPIRATION_TIME = 3600000; // 1 hour
-	//public static final String PASSWORD_RESET_URL = "/users/password-reset";
-	//public static final String H2_CONSOLE = "/h2-console/**";
-	
 	public static final long EXPIRATION_TIME = 1800000; 
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
