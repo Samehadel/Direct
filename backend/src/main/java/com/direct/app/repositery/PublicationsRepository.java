f5c54fe7c0cfacde9dfc8607ed019847d71f1731
Sameh_Adel <sameh.adel_20@outlook.com>
2022-04-30 12:22:32 +0200

    Added PublicationController Integration Tests

--- backend/src/main/java/com/direct/app/repositery/PublicationsRepository.java
+++ backend/src/main/java/com/direct/app/repositery/PublicationsRepository.java
@@ -1,11 +1,11 @@
 package com.direct.app.repositery;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.direct.app.io.entities.PublicationEntity;
@@ -22,4 +22,8 @@ public interface PublicationsRepository extends CrudRepository<PublicationEntity
 	@Modifying
 	@Query(value = "UPDATE publications pub SET pub.is_read=false WHERE pub.id=:id", nativeQuery = true)
 	public int markPublicationAsUnRead(long id);
+
+	@Query("SELECT publication FROM PublicationEntity publication " +
+			"WHERE publication.sender.id = :senderId")
+	public Set<PublicationEntity> findPublicationsBySenderId(Long senderId);
 }
