e2af9ebb723cb58b51ca034f8618c980cb306682
Sameh_Adel <sameh.adel_20@outlook.com>
2022-05-14 15:37:23 +0200

    Extracted Profile Image Controller from Profiles Controller

--- backend/src/main/java/com/direct/app/ui/controller/ProfileImageController.java
+++ backend/src/main/java/com/direct/app/ui/controller/ProfileImageController.java
@@ -1,34 +1,38 @@
 package com.direct.app.ui.controller;
 
 import com.direct.app.service.ProfileImageService;
-import com.direct.app.service.ProfilesService;
 import com.direct.app.shared.dto.ProfileImageDTO;
-import com.direct.app.shared.dto.ProfileDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Set;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
-@RequestMapping("/profiles")
+@RequestMapping("/profile")
 @CrossOrigin(value = "http://localhost:3000", allowedHeaders = "*")
-public class ProfilesController {
-
+public class ProfileImageController {
 	@Autowired
-	private ProfilesService profilesService;
+	private ProfileImageService profileImageService;
 
-	@GetMapping
-	public ResponseEntity findSimilarProfilesForUser() throws Exception {
-		Set<ProfileDto> similarUsers = profilesService.retrieveSimilarUsers();
+	@PostMapping(value = "/image", produces = APPLICATION_JSON_VALUE, consumes = MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity setProfileImage(@RequestPart MultipartFile imageFile) throws Exception {
+		String imageUrl = profileImageService.setProfileImage(imageFile);
 
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(similarUsers);
+				.body(imageUrl);
+	}
+
+	@GetMapping("/image")
+	public ResponseEntity getProfileImage() throws Exception {
+		ProfileImageDTO profileImageDTO = profileImageService.getProfileImage();
+
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(profileImageDTO);
 	}
 }
