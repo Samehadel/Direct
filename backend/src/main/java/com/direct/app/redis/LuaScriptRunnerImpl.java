0ba8cadbdce2cd78136dddda2d0840b88c53b821
Sameh_Adel <sameh.adel_20@outlook.com>
2023-07-08 16:48:16 +0300

    Apply Thread Safety and Redis Script Caching to LuaScriptRunner

--- backend/src/main/java/com/direct/app/redis/LuaScriptRunnerImpl.java
+++ backend/src/main/java/com/direct/app/redis/LuaScriptRunnerImpl.java
@@ -5,6 +5,7 @@ import io.lettuce.core.RedisClient;
 import io.lettuce.core.ScriptOutputType;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
+import net.jcip.annotations.ThreadSafe;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +13,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@ThreadSafe
 @Service
 public class LuaScriptRunnerImpl implements LuaScriptRunner {
 	private final Logger logger = LogManager.getLogger(RedisTemplateHolderImpl.class);
+	private final ConcurrentMap<String, String> scriptsSHA = new ConcurrentHashMap<>();
 
 	@Autowired
 	private RedisClient redisClient;
 
+
 	@Override
-	public Object executeLuaScript(String scriptName, ScriptOutputType returnType, String[] keys, String... arg) {
+	public Object executeLuaScript(String scriptName, ScriptOutputType returnType, String[] keys, String... args) {
 		String luaScript = getLuaScript(scriptName);
 
 		if (luaScript == null) {
@@ -30,7 +37,12 @@ public class LuaScriptRunnerImpl implements LuaScriptRunner {
 
 		try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
 			RedisCommands<String, String> commands = connection.sync();
-			Object result = commands.eval(luaScript, returnType, keys, arg);
+			String sha = scriptsSHA.get(scriptName);
+			if (sha == null) {
+				sha = commands.scriptLoad(luaScript);
+				scriptsSHA.put(scriptName, sha);
+			}
+			Object result = commands.evalsha(sha, returnType, keys, args);
 
 			return result;
 		} catch (Exception e) {
